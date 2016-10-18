70390f45538cf5d7433bf1a95c2247c06f222d84
Pedro Zaroni <zaronipedro@outlook.com>
2016-10-17 22:57:51 -0200

    SucataLivre(v0.1)

---
+++ sl from scratch/js/index.js
@@ -0,0 +1,23 @@
+$('input[type="submit"]').mousedown(function(){
+  $(this).css('background', '#333');
+});
+$('input[type="submit"]').mouseup(function(){
+  $(this).css('background', '#093');
+});
+
+$('#loginform').click(function(){
+  $('.login').fadeToggle('slow');
+  $(this).toggleClass('green');
+});
+
+$(document).mouseup(function (e)
+{
+    var container = $(".login");
+
+    if (!container.is(e.target) // if the target of the click isn't the container...
+        && container.has(e.target).length === 0) // ... nor a descendant of the container
+    {
+        container.hide();
+        $('#loginform').removeClass('green');
+    }
+});
